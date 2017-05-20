6a687310bee45026184ef7a0469b11d65046bf4e
kafeikelele <1046641572@qq.com>
2017-05-21 02:23:54 +0800

    Initial commit

---
+++ 1.py
@@ -0,0 +1,25 @@
+#_*_coding:utf8_*_
+print "hello wold"
+#字符串可以用"+"连接起来，for example:
+print 'hello ' + ',' + ' world'
+
+'''
+>>> a = '2'
+>>> print a
+2
+>>> b = 2
+>>> print b
+2
+>>> print a + b
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: cannot concatenate 'str' and 'int' objects
+
+>>> print int(a) + b
+4
+
+变量定义的原则：
+1、只能包含英文字母、数字、下划线
+2、首字母必须是英文字母或者下划线
+'''
+
