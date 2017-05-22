a11a89d20a85819da70589c067eb850f057bf535
wxh <1046641572@qq.com>
2017-05-23 01:56:33 +0800

    add 3.py test

---
+++ 4.py
@@ -0,0 +1,6 @@
+#_*_coding:utf8_*_
+# 练习：从命令行输入两个数字，打印出两个数字的和
+num1 = raw_input('Please input the number you want: ')
+num2 = raw_input('Please input the number you want: ')
+s = int(num1) + int(num2)
+print s
