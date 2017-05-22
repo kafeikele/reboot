a34d533d13bba7117cd536eab01983b528be2a05
wxh <1046641572@qq.com>
2017-05-23 02:58:44 +0800

    add

--- first/5.py
+++ first/5.py
@@ -5,6 +5,8 @@ b = 5
 print a == b
 print a != b
 
+#逻辑或、且   或是or  且是and
+
 '''
 脚本执行结果
 C:\Python27\python.exe C:/python/reboot/first/5.py
---
+++ first/6.py
@@ -0,0 +1,27 @@
+#_*_coding:utf8_*_
+#练习：输入两个人的年龄，判断大小
+wdh_age = raw_input('Please input your age: ')
+wxh_age = raw_input('Please input your age: ')
+if wdh_age > wxh_age :
+    print 'wdh_age > wxh_age'
+else:
+    if wdh_age == wxh_age :
+        print 'wdh_age = wxh_age'
+    else:
+        print 'wdh_age < wxh_age'
+
+
+if wdh_age > wxh_age :
+    print 'wdh_age > wxh_age'
+elif wdh_age == wxh_age :
+    print 'wdh_age = wxh_age'
+else:
+     print 'wdh_age < wxh_age'
+
+'''
+C:\Python27\python.exe C:/python/reboot/first/6.py
+Please input your age: 23
+Please input your age: 25
+wdh_age < wxh_age
+
+'''
