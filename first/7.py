d43888c126b0da9ab4c0ba6a6008cfa51bd17d5e
wxh <1046641572@qq.com>
2017-05-24 01:07:22 +0800

    add

---
+++ first/7.py
@@ -0,0 +1,18 @@
+#_*_coding:utf8_*_
+#如果age_1的值小于age_2 ,则两个值进行替换，age_1始终显示为最大值
+age_1 = raw_input('Please input the age_1: ')
+age_2 = raw_input('Please input the age_2: ')
+if  age_1 < age_2:
+    tmp = age_1
+    age_1 = age_2
+    age_2 = tmp
+    print 'age_1:%s age_2:%s' %(age_1,age_2)
+
+'''
+输出结果：
+C:\Python27\python.exe C:/python/reboot/first/7.py
+Please input the age_1: 12
+Please input the age_2: 13
+age_1:13 age_2:12
+
+'''
